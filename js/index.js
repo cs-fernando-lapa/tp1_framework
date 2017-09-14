ab7977db52389c9f17d6c309475260337ff8d980
Fernando Lapa <fernando.lapa@concrete.com.br>
2017-09-14 15:50:03 -0300

    Preparando estrutura de pastas, adicionando UIRouter

--- js/index.js
+++ js/index.js
@@ -1,33 +1,33 @@
-  (function(){
-    angular.module('myApp', [])
-    .controller('myController', ['$scope', function($scope){
-      this.tarefas = [
-        { nome: 'Varrer o quarto', categoria: 'Casa', feito: false },
-        { nome: 'Lavar a louça', categoria: 'Casa', feito: true },
-        { nome: 'Lavar as roupas', categoria: 'Casa', feito: false },
-        { nome: 'Consertar a porta', categoria: 'Casa', feito: false },
-        { nome: 'Terminar o relatório', categoria: 'Trabalho', feito: false },
-        { nome: 'Estudar para a próxima reunião', categoria: 'Trabalho', feito: false },
-        { nome: 'Enviar email sobre o problema', categoria: 'Trabalho', feito: false },
-        { nome: 'Levar pó de café', categoria: 'Trabalho', feito: true },
-        { nome: 'Comprar canetas novas', categoria: 'Trabalho', feito: true },
-        { nome: 'Estudar Etapa 1', categoria: 'Faculdade', feito: true },
-        { nome: 'Estudar Etapa 2', categoria: 'Faculdade', feito: true },
-        { nome: 'Estudar Etapa 3', categoria: 'Faculdade', feito: false },
-        { nome: 'Fazer TP1', categoria: 'Faculdade', feito: false }
-      ]
-      this.categorias = ['Casa', 'Trabalho', 'Faculdade'];
+(function(){
+    var app = angular.module('myApp', ['ui.router']);
 
-      this.plusCategoria = function(){
-        this.categorias.push(this.categoria)
-      }
+    app.config(function($stateProvider, $urlRouterProvider){
 
-      this.plusTarefa = function(){
-        this.tarefas.push({
-          nome:this.nome,
-          categoria:this.seleciona,
-          feito:false
-        })
-      }
-    }]);
-  })();
+      $urlRouterProvider.otherwise("/");
+
+      $stateProvider.state('home',{
+        url: "/",
+        template:
+                <div>
+                  <h1> Selecione uma opção</h1>
+                </div>
+      });
+      $stateProvider.state('categoria',{
+        url: "/categoria",
+        templateUrl: "categoria.htm"
+      });
+      $stateProvider.state('categoria+',{
+        url: "/addcategoria",
+        templateUrl: "categoria+.htm"
+      });
+      $stateProvider.state('resposta',{
+        url: "/aswer",
+        templateUrl: "respostaEmail.htm"
+      });
+      $stateProvider.state('tarefas',{
+        url: "/tarefas",
+        templateUrl: "tarefas.htm"
+      });
+    });
+  }
+)();
